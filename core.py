2e3e9a4b38e097dd743be12d9c6fc77cb326ac9e
westhecool <52859033+westhecool@users.noreply.github.com>
2023-09-04 10:27:01 -0400

    update core

--- core.py
+++ core.py
@@ -1,8 +1,9 @@
 import hashlib
 import os
 import math
+from cryptography.fernet import Fernet
 
-def HashFile(file, BUF_SIZE = 1024 * 1024): # 1MB chunks
+def hashFile(file, BUF_SIZE = 1024 * 1024): # 1MB chunks
     hash = hashlib.sha256()
     with open(file, 'rb') as f:
         while True:
@@ -19,9 +20,26 @@ class FileChunkReader:
         self.basename = os.path.basename(file)
         self.filesize = os.path.getsize(file)
         self.chunkcount = math.ceil(self.filesize / self._chunksize)
-    def ReadChunk(self, chunk):
+    def readChunk(self, chunk):
         if chunk > self.chunkcount or chunk < 1:
             raise Exception('chunk index out of range!')
         start = self._chunksize * (chunk - 1)
         self._file.seek(start)
         return self._file.read(self._chunksize)
+
+class cypto:
+    def __init__(self):
+        self._key = False
+    def genKey(self):
+        return Fernet.generate_key()
+    def setKey(self, key):
+        self._key = key
+        self._fernet = Fernet(key)
+    def encrypt(self, data):
+        if not self._key:
+            raise Exception('no key has been set!')
+        return self._fernet.encrypt(data)
+    def decrypt(self, data):
+        if not self._key:
+            raise Exception('no key has been set!')
+        return self._fernet.decrypt(data)
