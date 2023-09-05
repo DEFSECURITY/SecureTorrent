1be6ac373783f7b22302a7d3fc1ab8b38bb6d4fe
mysterek_ <59736397+mysterek1337@users.noreply.github.com>
2023-09-05 22:34:31 +0200

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,7 +1,9 @@
 # Usage of CTkMessagebox instead of normal's tkinter's 
 
 # Known bugs:
+# No development builds. (due to my huge amount of pull requests)
 # (INITIALIZE) CTkMessagebox opens up a new screen. Fix this
+
 import requests
 import client
 from CTkMessagebox import CTkMessagebox
@@ -13,10 +15,11 @@ def Update():
         LatestBuild = int(response.text.strip())
         print("CLI | Current downloaded build:",client.CurrentVersion,"and the latest one is:",LatestBuild)
         if client.CurrentVersion != LatestBuild:
-            msg = CTkMessagebox(title="Available update", message=f"Theres a newer version available \nDo you want to download it? \nCurrent Version: {client.CurrentVersion}\nLatest: {LatestBuild}",
+            msg = CTkMessagebox(title="Available stable update", message=f"Theres a newer version available \nDo you want to download it? \nCurrent Version: {client.CurrentVersion}\nLatest: {LatestBuild}",
                             icon="question", option_1="No", option_2="Yes")
             response = msg.get()
             if response=="Yes":
+               # Maybe in the future, automatic updater?
                webbrowser.open("https://github.com/DEFSecurity/SafeTorrenting/releases/")
             else:
                print("CLI | Exiting Updater")
